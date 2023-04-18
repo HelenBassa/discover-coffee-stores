e8c0cf55e5bd582c732030db42640fe163cf693d
HelenBassa <helenbassa@gmail.com>
2023-04-18 22:07:29 +0300

    Refactor and styling Coffee Store Page

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,13 +1,91 @@
 import { useRouter } from "next/router";
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+import cls from "classnames";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "@/data/coffee-stores.json";
+
+import styles from "@/styles/CoffeeStores.module.css";
+
+export function getStaticProps(staticProps) {
+  const params = staticProps.params;
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+        return coffeeStore.id === +params.id; //convert to number
+      }),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  const paths = coffeeStoresData.map((coffeeStore) => {
+    return {
+      params: {
+        id: coffeeStore.id + "", //convert to string
+      },
+    };
+  });
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+const CoffeeStore = (props) => {
   const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading ...</div>;
+  }
+
+  const { name, address, neighbourhood, imgUrl } = props.coffeeStore;
+
+  const handleUpVoteButton = () => {
+    console.log("handle upvote");
+  };
+
   return (
-    <div>
-      Coffee Store Page {router.query.id}
-      <Link href="/">Back to Home</Link>
-      <Link href="/coffee-store/dynamic">Go to page dynamic</Link>
+    <div className={styles.layout}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href="/">Back to Home</Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>Name: {name}</h1>
+          </div>
+          <Image
+            className={styles.storeImg}
+            alt={name}
+            src={imgUrl}
+            width={600}
+            height={360}
+          />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/places.svg" width={24} height={24} />
+            <p className={styles.text}>Address: {address}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/nearMe.svg" width={24} height={24} />
+            <p className={styles.text}>{neighbourhood}</p>
+          </div>
+          <div className={styles.iconWrapper}>
+            <Image src="/static/icons/star.svg" width={24} height={24} />
+            <p className={styles.text}>1</p>
+          </div>
+          <button className={styles.upVoteButton} onClick={handleUpVoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
