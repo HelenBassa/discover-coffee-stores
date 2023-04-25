feedbbb1432a325522c83afd9b165ad17c9df48d
HelenBassa <helenbassa@gmail.com>
2023-04-25 11:33:32 +0300

    getCoffeeStoreById, findRecordByFilter

---
+++ pages/api/getCoffeeStoreById.js
@@ -0,0 +1,26 @@
+import { findRecordByFilter } from "@/lib/airtable";
+
+const getCoffeeStoreById = async (req, res) => {
+  const { id } = req.query;
+
+  try {
+    if (id) {
+      const records = await findRecordByFilter(id);
+
+      if (records.length !== 0) {
+        res.json(records);
+      } else {
+        res.json({ message: `Id could not be found` });
+      }
+    } else {
+      res.status(400);
+      res.json({ message: "Id or name is missing" });
+    }
+  } catch (error) {
+    res.status(500);
+    res.json({ message: "Something went wrong", error });
+    console.error(error);
+  }
+};
+
+export default getCoffeeStoreById;
