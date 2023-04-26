190950a287e4d5edfb0d1dfe2088808a53fcf07e
HelenBassa <helenbassa@gmail.com>
2023-04-26 22:39:30 +0300

    add hero-image.webp

--- pages/index.js
+++ pages/index.js
@@ -90,7 +90,7 @@ export default function Home(props) {
           <Image
             className={styles.heroImg}
             alt="hero"
-            src="/static/hero-image.png"
+            src="/static/hero-image.webp"
             width={600}
             height={300}
             priority
